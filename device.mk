9de645fc1ca4e8c2cb67f364b291af5769492e58
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-16 00:42:47 -0300

    Reverted to ligui shim

--- device.mk
+++ device.mk
@@ -157,7 +157,8 @@ PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-impl \
     android.hardware.camera.provider@2.4-service \
     vendor.qti.hardware.camera.device@1.0 \
-    camera_shims \
+    libfui \
+    libui_shim \
     libgui.vendor
     
 # Component overrides
