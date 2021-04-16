4a469d24fe923422f9d886aa4b94a262037c6bad
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-16 13:04:42 -0300

    Update aosp_A001D.mk

--- aosp_A001D.mk
+++ aosp_A001D.mk
@@ -35,11 +35,3 @@ PRODUCT_NAME := aosp_A001D
 BOARD_VENDOR := asus
 PRODUCT_BRAND := asus
 PRODUCT_MODEL := ASUS_A001D
-
-# PixelPlusUI Maintainer
-PRODUCT_PROPERTY_OVERRIDES += \
-  ro.ppui.device_name=Max Shot \
-  ro.ppui.version=3.4 \
-  ro.ppui.version_code=A001D \
-  ro.ppui.is_official=true \
-  ro.ppui.maintainer_name=mathpfvr
