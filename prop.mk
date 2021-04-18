e0f3097ed18df6fa15a6421ed3967e90282c2719
Matheus Vinicius <matheussouzas65@gmail.com>
2021-04-17 22:56:04 -0300

    Update prop.mk

--- prop.mk
+++ prop.mk
@@ -20,12 +20,59 @@ PRODUCT_PROPERTY_OVERRIDES += \
     qcom.hw.aac.encoder=false \
     vendor.audio.feature.incall_music.enable=true
 
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.calfile0=/vendor/etc/acdbdata/Bluetooth_cal.acdb \
+    persist.vendor.audio.calfile1=/vendor/etc/acdbdata/General_cal.acdb \
+    persist.vendor.audio.calfile2=/vendor/etc/acdbdata/Global_cal.acdb \
+    persist.vendor.audio.calfile3=/vendor/etc/acdbdata/Handset_cal.acdb \
+    persist.vendor.audio.calfile4=/vendor/etc/acdbdata/Hdmi_cal.acdb \
+    persist.vendor.audio.calfile5=/vendor/etc/acdbdata/Headset_cal.acdb \
+    persist.vendor.audio.calfile6=/vendor/etc/acdbdata/Speaker_cal.acdb \
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.vendor.audio.fluence.speaker=false \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicecomm=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    ro.qc.sdk.audio.fluencetype=none \
+    ro.qc.sdk.audio.ssr=false \
+    vendor.use.voice.path.for.pcm.voip=false \
+    ro.config.media_vol_steps=25 \
+    ro.config.vc_call_vol_steps=7
+
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
-bluetooth.hfp.client=1 \
-vendor.qcom.bluetooth.soc=smd \
-ro.bluetooth.hfp.ver=1.7 \
-ro.qualcomm.bt.hci_transport=smd
+    bluetooth.hfp.client=1 \
+    qcom.bt.le_dev_pwr_class=1 \
+    ro.cutoff_voltage_mv=3400 \
+    ro.bluetooth.hfp.ver=1.7 \
+    qcom.bluetooth.soc=smd \
+    vendor.qcom.bluetooth.soc=smd \
+    ro.qualcomm.bt.hci_transport=smd \
+    ro.qualcomm.bluetooth.opp=true \
+    ro.qualcomm.bluetooth.hfp=true \
+    ro.qualcomm.bluetooth.hsp=true \
+    ro.qualcomm.bluetooth.pbap=true \
+    ro.qualcomm.bluetooth.ftp=true \
+    ro.qualcomm.bluetooth.nap=true \
+    ro.bluetooth.sap=true \
+    ro.bluetooth.dun=true \
+    ro.qualcomm.bluetooth.map=true \
+    persist.bt.enableAptXHD=true \
+
+# FM
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.hw.fm.init=0
+
+PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    vendor.bluetooth.soc=smd \
+    ro.fm.transmitter=false \
+    ro.vendor.fm.use_audio_session=true
+
+# Boot
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.vendor.shutdown.waittime=500 \
+    ro.build.shutdown_timeout=0
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -48,17 +95,33 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.camera.time.monotonic=1 \
     persist.camera.eis.enable=1
 
-# Cne
+# CNE
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.cne.feature=1
+    persist.vendor.cne.logging.qxdm=3974 \
+    persist.vendor.cne.rat.wlan.chip.oem=WCN \
+    persist.vendor.sys.cnd.iwlan=1
 
-# Coresight
+# Property to enable fingerprint
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.debug.coresight.config=stm-events
+    persist.qfp=false
 
-# Dalvik dex2oat
+# Core_ctrl
 PRODUCT_PROPERTY_OVERRIDES += \
-dalvik.vm.dex2oat64.enabled=true
+    ro.vendor.qti.core_ctl_min_cpu=2 \
+    ro.vendor.qti.core_ctl_max_cpu=4
+
+# Dalvik
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.heapgrowthlimit=256m \
+    dalvik.vm.heapmaxfree=8m \
+    dalvik.vm.heapminfree=512k \
+    dalvik.vm.heapsize=512m \
+    dalvik.vm.heapstartsize=8m \
+    dalvik.vm.heaptargetutilization=0.75 \
+    ro.sys.fw.dex2oat_thread_count=4 \
+    dalvik.vm.boot-dex2oat-threads=8 \
+    dalvik.vm.dex2oat-threads=4 \
+    dalvik.vm.image-dex2oat-threads=4
 
 # Display
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -87,80 +150,67 @@ PRODUCT_PROPERTY_OVERRIDES += \
     persist.debug.wfd.enable=1 \
     persist.sys.wfd.virtual=0
 
-# DPM
+# HWUI
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.dpm.loglevel=0 \
-persist.vendor.dpm.nsrm.bkg.evt=3955 \
-persist.vendor.dpmhalservice.enable=1
+    ro.hwui.texture_cache_size=72 \
+    ro.hwui.layer_cache_size=48 \
+    ro.hwui.r_buffer_cache_size=8 \
+    ro.hwui.path_cache_size=32 \
+    ro.hwui.gradient_cache_size=1 \
+    ro.hwui.drop_shadow_cache_size=6 \
+    ro.hwui.texture_cache_flushrate=0.4 \
+    ro.hwui.text_small_cache_width=1024 \
+    ro.hwui.text_small_cache_height=1024 \
+    ro.hwui.text_large_cache_width=2048 \
+    ro.hwui.text_large_cache_height=1024
 
-# DRM
+# IMS
 PRODUCT_PROPERTY_OVERRIDES += \
-drm.service.enabled=true
-
-# Fingerprint
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.qfp=false
-
-# Core_ctrl
-PRODUCT_PROPERTY_OVERRIDES += \
-    ro.vendor.qti.core_ctl_min_cpu=2 \
-    ro.vendor.qti.core_ctl_max_cpu=4
-
-# Fm
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.fm.transmitter=false
-
-# Frp
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.frp.pst=/dev/block/bootdevice/by-name/config
-
-# GPS
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.gps.qc_nlp_in_use=1 \
-persist.loc.nlp_name=com.qualcomm.location \
-ro.gps.agps_provider=1
+    persist.vendor.radio.jbims=1 \
+    persist.radio.RATE_ADAPT_ENABLE=1 \
+    persist.radio.VT_ENABLE=1 \
+    persist.radio.VT_HYBRID_ENABLE=1 \
+    persist.radio.VT_USE_MDM_TIME=0 \
+    persist.ims.disableADBLogs=2 \
+    persist.ims.disableDebugLogs=0 \
+    persist.ims.disableIMSLogs=0 \
+    persist.ims.disableQXDMLogs=0 \
+    persist.ims.rcs=false \
+    persist.ims.volte=true \
+    persist.ims.vt=false \
+    persist.ims.vt.epdg=false \
+    persist.mm.sta.enable=0 \
+    persist.vt.supported=0 \
+    persist.volte_enabled_by_hw=1 \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.ims_volte_enable=1 \
+    persist.dbg.vt_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.vendor.radio.force_ltd_sys_ind=1 \
+    persist.vendor.radio.voice_on_lte=1 \
+    persist.vendor.radio.calls.on.ims=1 \
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
-av.debug.disable.pers.cache=1 \
-debug.stagefright.omx_default_rank=0 \
-media.msm8956hw=0 \
-media.stagefright.audio.sink=280 \
-vendor.mm.enable.qcom_parser=1048575 \
-media.stagefright.thumbnail.prefer_hw_codecs=true \
-mmp.enable.3g2=true \
-vendor.audio.hw.aac.encoder=true \
-vendor.vidc.debug.level=0 \
-vendor.vidc.dec.downscalar_height=1088 \
-vendor.vidc.dec.downscalar_width=1920 \
-vendor.vidc.disable.split.mode=1 \
-vendor.vidc.enc.disable.pq=true \
-vendor.vidc.enc.disable_bframes=1 \
-vendor.video.disable.ubwc=1
+    media.aac_51_output_enabled=true \
+    mm.enable.qcom_parser=135715 \
+    mm.enable.sec.smoothstreaming=false \
+    mm.enable.smoothstreaming=false \
+    mmp.enable.3g2=true \
+    drm.service.enabled=true \
+    media.stagefright.thumbnail.prefer_hw_codecs=true
 
-
-# Perf
+# NITZ
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.extension_library=libqti-perfd-client.so \
-vendor.perf.gestureflingboost.enable=true
-
-# Netmgrd
-PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.use_data_netmgrd=true \
-persist.data.netmgrd.qos.enable=true \
-persist.vendor.data.mode=concurrent
-
-# Nitz
-PRODUCT_PROPERTY_OVERRIDES += \
-persist.rild.nitz_plmn="" \
-persist.rild.nitz_long_ons_0="" \
-persist.rild.nitz_long_ons_1="" \
-persist.rild.nitz_long_ons_2="" \
-persist.rild.nitz_long_ons_3="" \
-persist.rild.nitz_short_ons_0="" \
-persist.rild.nitz_short_ons_1="" \
-persist.rild.nitz_short_ons_2="" \
-persist.rild.nitz_short_ons_3=""
+    persist.rild.nitz_plmn="" \
+    persist.rild.nitz_long_ons_0="" \
+    persist.rild.nitz_long_ons_1="" \
+    persist.rild.nitz_long_ons_2="" \
+    persist.rild.nitz_long_ons_3="" \
+    persist.rild.nitz_short_ons_0="" \
+    persist.rild.nitz_short_ons_1="" \
+    persist.rild.nitz_short_ons_2="" \
+    persist.rild.nitz_short_ons_3=""
 
 # Qualcomm
 PRODUCT_PROPERTY_OVERRIDES += \
@@ -170,68 +220,142 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Radio
 PRODUCT_PROPERTY_OVERRIDES += \
-DEVICE_PROVISIONED=1 \
-persist.dbg.volte_avail_ovr=1 \
-persist.dbg.vt_avail_ovr=1 \
-persist.dbg.wfc_avail_ovr=1 \
-persist.vendor.radio.apm_sim_not_pwdn=1 \
-persist.radio.aosp_usr_pref_sel=true \
-persist.radio.multisim.config=dsds \
-persist.vendor.radio.custom_ecc=1 \
-persist.vendor.radio.procedure_bytes=SKIP \
-persist.vendor.radio.rat_on=combine \
-persist.vendor.radio.sib16_support=1 \
-persist.vendor.data.iwlan.enable=true \
-ril.subscription.types=NV,RUIM \
-rild.libargs=-d/dev/smd0 \
-rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
-ro.telephony.call_ring.multiple=false \
-ro.telephony.default_network=22,22 \
-ro.telephony.iwlan_operation_mode=legacy \
-service.qti.ims.enabled=1
+    persist.radio.apn_delay=5000 \
+    persist.radio.adam=true \
+    persist.sys.fflag.override.settings_network_and_internet_v2=true \
+    persist.vendor.radio.dfr_mode_set=1 \
+    persist.vendor.radio.force_get_pref=1 \
+    persist.vendor.radio.no_wait_for_card=1 \
+    persist.vendor.radio.oem_ind_to_both=0 \
+    persist.vendor.radio.relay_oprt_change=1 \
+    rild.libpath=/vendor/lib64/libril-qc-qmi-1.so \
+    ro.mot.ignore_csim_appid=true \
+    persist.sys.ssr.restart_level=ALL_ENABLE \
+    persist.sys.qc.sub.rdump.on=1 \
+    persist.vendor.radio.sw_mbn_update=0 \
+    persist.sys.qc.sub.rdump.max=3 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.is_wps_enabled=true \
+    persist.vendor.radio.mt_sms_ack=30 \
+    persist.vendor.radio.0x9e_not_callname=1 \
+    persist.vendor.qcril_uim_vcc_feature=1 \
+    persist.mot.gps.conf.from.sim=true \
+    persist.net.doxlat=true \
+    persist.radio.ROTATION_ENABLE=1 \
+    persist.radio.adb_log_on=0 \
+    persist.radio.calls.on.ims=true \
+    persist.radio.sar_sensor=1 \
+    persist.vendor.radio.sib16_support=1 \
+    persist.radio.sib16_support=1 \
+    persist.vendor.radio.rat_on=combine \
+    ro.telephony.call_ring.multiple=false \
+    persist.vendor.radio.eri64_as_home=1 \
+    persist.vendor.radio.data_con_rprt=1 \
+    persist.radio.calls.on.ims=1 \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.dpm.feature=0 \
+    persist.vendor.radio.force_on_dc=true \
+    persist.radio.custom_ecc=1 \
+    persist.vendor.radio.data_ltd_sys_ind=1 \
+    persist.vendor.radio.ignore_dom_time=10 \
+    persist.radio.ignore_dom_time=10 \
+    persist.radio.is_wps_enabled=true \
+    persist.radio.videopause.mode=1 \
+    persist.radio.sap_silent_pin=1 \
+    persist.radio.always_send_plmn=true \
+    persist.rcs.supported=1 \
+    ro.telephony.default_network=10,0 \
+    persist.radio.msgtunnel.start=true \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.qcril_uim_vcc_feature=1 \
+    persist.radio.schd.cache=3500 \
+    persist.vendor.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.lte_vrte_ltd=1 \
+    persist.vendor.radio.cs_srv_type=1 \
+    ro.use_data_netmgrd=true \
+    ro.vendor.use_data_netmgrd=true \
+    persist.data.qmi.adb_logmask=0 \
+    persist.vendor.radio.snapshot_timer=22 \
+    persist.vendor.radio.snapshot_enabled=1 \
+    persist.radio.domain.ps=0 \
+    persist.rmnet.mux=enabled \
+    persist.radio.REVERSE_QMI=0 \
+    persist.cne.feature=1 \
+    persist.data.netmgrd.qos.enable=true \
+    persist.data.mode=concurrent \
+    persist.vendor.data.mode=concurrent \
+    persist.data.iwlan.enable=true \
+    persist.sys.cnd.iwlan=1 \
+    persist.cne.logging.qxdm=3974 \
+    persist.vendor.ims.disableDebugLogs=1 \
+    persist.vendor.ims.disableQXDMLogs=1 \
+    DEVICE_PROVISIONED=1
 
-# Radio (IMS)
+# RmNet Data
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.vendor.ims.disableIMSLogs=1 \
-persist.vendor.ims.disableADBLogs=1 \
-persist.vendor.ims.disableDebugDataPathLogs=1 \
-persist.vendor.ims.disableDebugLogs=1 \
-persist.vendor.ims.disableSigHandler=1 \
-persist.vendor.ims.disableQXDMLogs=1
+    persist.rmnet.data.enable=true \
+    persist.data.wda.enable=true \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.ul_mode=5 \
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.dev_name=rmnet_usb0
 
-# SurfaceFlinger
-PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-ro.surface_flinger.protected_contents=true \
-ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
-ro.surface_flinger.max_virtual_display_dimension=4096 \
-ro.surface_flinger.vsync_event_phase_offset_ns=2000000 \
-ro.surface_flinger.vsync_sf_event_phase_offset_ns=6000000 \
-ro.surface_flinger.max_frame_buffer_acquired_buffers=3
-
-# Time Services
+#Enable B service adj transition by default
 PRODUCT_PROPERTY_OVERRIDES += \
-persist.timed.enable=true
+    ro.vendor.qti.sys.fw.bservice_enable=true \
+    ro.vendor.qti.sys.fw.bservice_limit=5 \
+    ro.vendor.qti.sys.fw.bservice_age=5000
 
 # Trim properties
 PRODUCT_PROPERTY_OVERRIDES += \
-ro.vendor.qti.sys.fw.use_trim_settings=true \
-ro.vendor.qti.sys.fw.empty_app_percent=50 \
-ro.vendor.qti.sys.fw.trim_empty_percent=100 \
-ro.vendor.qti.sys.fw.trim_cache_percent=100 \
-ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
+    ro.vendor.qti.sys.fw.use_trim_settings=true \
+    ro.vendor.qti.sys.fw.empty_app_percent=50 \
+    ro.vendor.qti.sys.fw.trim_empty_percent=100 \
+    ro.vendor.qti.sys.fw.trim_cache_percent=100 \
+    ro.vendor.qti.sys.fw.trim_enable_memory=2147483648
 
-# Tcp
+# USB
 PRODUCT_PROPERTY_OVERRIDES += \
-net.tcp.2g_init_rwnd=10
+    ro.usb.mtp=0x2e82 \
+    ro.usb.mtp_adb=0x2e76 \
+    ro.usb.ptp=0x2e83 \
+    ro.usb.ptp_adb=0x2e84 \
+    ro.usb.bpt=0x2ee5 \
+    ro.usb.bpt_adb=0x2ee6 \
+    ro.usb.bpteth=0x2ee7 \
+    ro.usb.bpteth_adb=0x2ee8 \
 
-# Wifi
+# Vendor Extension
 PRODUCT_PROPERTY_OVERRIDES += \
-wifi.interface=wlan0
+    ro.vendor.extension_library=libqti-perfd-client.so \
+    ro.vendor.at_library=libqti-at.so \
+    ro.vendor.gt_library=libqti-gt.so
 
-# USAP
-PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
-persist.device_config.runtime_native.usap_pool_enabled=true
+# SurfaceFlinger
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=20500000 \
+    debug.sf.early.sf.duration=21000000 \
+    debug.sf.early.app.duration=16500000 \
+    debug.sf.earlyGl.sf.duration=13500000 \
+    debug.sf.earlyGl.app.duration=21000000
 
+
+# Camera Aux Package list
+PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.camera.aux.packagelist=com.google.android.GoogleCamera,com.google.android.apps.cameraslite,org.codeaurora.snapcam
+
+# Fast App Launch
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.device_config.runtime_native.usap_pool_enabled=true
+
+# UI
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.use_fifo_ui=1
 
 # Tweak the memory management of the device, enable more background apps.. etc..
 PRODUCT_PROPERTY_OVERRIDES += \
